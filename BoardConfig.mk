1678d313fb1e0d2234dbfa181e0d7dbde9fb45a8
Tony Layher <layhertony@gmail.com>
2011-10-19 13:43:10 -0400

    Hercules: Fix up Board defines.
    *Added audio defines
    *Fixed up wifi defines
    *Added BT
    *Fixed up filesystem defines and sizes
    *Fixed include define
    *Added QCOM specifics
    *Added FM Radio

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,7 +3,7 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/samsung/hercules/BoardConfigVendor.mk
 
-GET_SPECIFIC_HEADER_PATH := device/samsung/hercules/include
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/hercules/include
 
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_KERNEL := false
@@ -22,9 +22,18 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8660_SURF
 ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
 TARGET_AVOID_DRAW_TEXTURE_EXTENSION := true
+TARGET_USES_C2D_COMPOSITION := true
 TARGET_HAVE_HDMI_OUT := true
 TARGET_USES_OVERLAY := true
 TARGET_HAVE_TSLIB := false
+
+BOARD_MOBILEDATA_INTERFACE_NAME = "rmnet_sdio0"
+BOARD_HAS_EXTRA_SYS_PROPS := true
+BOARD_USES_QCOM_LIBS := true
+BOARD_USES_QCOM_LIBRPC := true
+BOARD_USES_QCOM_GPS := true
+BOARD_USE_QCOM_PMEM := true
+BOARD_CAMERA_USE_GETBUFFERINFO := true
 
 BOARD_EGL_CFG := device/samsung/hercules/egl.cfg
 
@@ -32,11 +41,11 @@ BOARD_EGL_CFG := device/samsung/hercules/egl.cfg
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
 WPA_SUPPLICANT_VERSION      := VER_0_6_X
 BOARD_WLAN_DEVICE           := bcm4329
-WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/bcm4329.ko"
-WIFI_DRIVER_FW_STA_PATH     := "/system/etc/firmware/fw_bcm4329.bin"
-WIFI_DRIVER_FW_AP_PATH      := "/system/etc/firmware/fw_bcm4329_apsta.bin"
-WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/firmware/fw_bcm4329.bin nvram_path=/proc/calibration"
-WIFI_DRIVER_MODULE_NAME     := "bcm4329"
+WIFI_DRIVER_MODULE_PATH     := "/system/lib/modules/dhd.ko"
+WIFI_DRIVER_FW_STA_PATH     := "/system/etc/wifi/bcm4330_sta.bin"
+WIFI_DRIVER_FW_AP_PATH      := "/system/etc/wifi/bcm4330_aps.bin"
+WIFI_DRIVER_MODULE_ARG      := "firmware_path=/system/etc/wifi/bcm4330_sta.bin nvram_path=system/etc/wifi/nvram_net.txt"
+WIFI_DRIVER_MODULE_NAME     := "dhd"
 
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_watchdog.appsbark=0 msm_watchdog.enable=1 loglevel=4 
 BOARD_KERNEL_BASE := 0x40400000
@@ -46,15 +55,30 @@ BOARD_FORCE_RAMDISK_ADDRESS := 0x41900000
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00A00000
-#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
-#BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 617193472
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2115760128
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 TARGET_PREBUILT_KERNEL := device/samsung/hercules/kernel
 
-TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
+# Audio
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_PREBUILT_LIBAUDIO := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_FORCE_STATIC_A2DP := true
+
+# FM Radio
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+BOARD_USE_BROADCOM_FM_VOLUME_HACK := true
+
+TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_CUSTOM_GRAPHICS:= ../../../device/samsung/hercules/recovery/graphics.c
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
